1ab67779d51ce2b625f63fdd31e8432b8a4b7612
William Chen <wxllchxn@umich.edu>
2019-10-12 00:24:02 -0400

    api test

---
+++ API/api.py
@@ -0,0 +1,16 @@
+# importing the requests library 
+import requests 
+
+# key
+key = "42e365dd904d4417a04c250f762d66c6"
+
+# api-endpoint 
+URL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2"
+
+# sending get request and saving the response as response object 
+r = requests.get(url = URL)
+  
+# extracting data in json format 
+data = r.json() 
+
+print(data)
