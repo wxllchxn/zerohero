6a2544ec98d42a86509783e197a3b90e76745594
William Chen <wxllchxn@umich.edu>
2019-10-12 03:13:30 -0400

    updated app

--- API/api.py
+++ API/api.py
@@ -5,7 +5,7 @@ import requests
 key = "42e365dd904d4417a04c250f762d66c6"
 
 # api-endpoint 
-URL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2"
+URL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2?apiKey="+key
 
 # sending get request and saving the response as response object 
 r = requests.get(url = URL)
---
+++ app/README.md
@@ -0,0 +1,6 @@
+# asl-translator
+Translates ASL into text and speech in real time. 
+
+[Devpost Link](https://devpost.com/software/translate-american-sign-language-translator)
+
+This repository contains the EXPO app. The model and cloud functions are stored on Google Cloud Platform. 
